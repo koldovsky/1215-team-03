742fdc28ffa98ef5946c05c5d639993ef1c6309a
ssashayurchenko <ssasha.yurchenko@gmail.com>
2024-06-19 14:37:22 +0300

    Added collective file with tasks

---
+++ js tasks/ tasks03.js
@@ -0,0 +1,15 @@
+//https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
+// https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+function makeNegative(num) {
+  return num > 0 ? -num : num;
+}
+
+// https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+
+function move(position, roll) {
+  return position + roll * 2;
+}
