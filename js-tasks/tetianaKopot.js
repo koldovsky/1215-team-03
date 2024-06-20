da153cd122063df72ae2209b3a2d9f37948c5e5b
TetyanaKopot <144814112+TetyanaKopot@users.noreply.github.com>
2024-06-20 16:30:31 +0000

    Added additional tasks

--- js-tasks/tetianaKopot.js
+++ js-tasks/tetianaKopot.js
@@ -45,3 +45,40 @@ function min(arr, toReturn) {
   const minValue = Math.min(...arr)
   return toReturn === 'value' ? minValue : arr.indexOf(minValue)
 }
+
+
+//***********Додаткові**************
+//https://www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript
+function doubleInteger(i) {
+  return i * 2
+}
+
+//https://www.codewars.com/kata/5b853229cfde412a470000d0/train/javascript
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  return Math.abs(dadYearsOld - sonYearsOld * 2)
+}
+
+//https://www.codewars.com/kata/5933a1f8552bc2750a0000ed/train/javascript
+function nthEven(n) {
+  return n * 2 - 2
+}
+
+//https://www.codewars.com/kata/574b3b1599d8f897470018f6/train/javascript
+function getRealFloor(n) {
+  if (n > 13) {
+    return n - 2
+  } else if (n > 0) {
+    return n - 1
+  }
+  return n
+}
+
+//https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/javascript
+function past(h, m, s) {
+  return s * 1000 + m * 60000 + h * 3600000
+}
+
+//https://www.codewars.com/kata/5545f109004975ea66000086/train/javascript
+function isDivisible(n, x, y) {
+  return n % x === 0 && n % y === 0
+}
