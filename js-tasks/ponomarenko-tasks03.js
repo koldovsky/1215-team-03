a5a8641f903bd7780d4c0a9e2ab78df95e4bc26c
ElkaKR <ponomarenkoalena555@gmail.com>
2024-06-22 17:08:03 +0000

    added additional tasks

--- js-tasks/ponomarenko-tasks03.js
+++ js-tasks/ponomarenko-tasks03.js
@@ -22,4 +22,45 @@ function giveMeFive(obj) {
     }
     return arr;
   }
-  
+ 
+//additional tasks
+// https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript
+function buildFun(n) {
+  let res = [];
+  for (var i = 0; i < n; i++) {
+    res.push(
+      (function (x) {
+        return function () {
+          return x;
+        };
+      })(i),
+    );
+  }
+  return res;
+} 
+
+//https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 0, "shark", status);
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 4, "cat", status);
+  }
+  introduce() {
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.  Meow meow!`;
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, status, master) {
+    super(name, age, 4, "dog", status);
+    this.master = master;
+  }
+  greetMaster() {
+    return `Hello ${this.master}`;
+  }
+}
