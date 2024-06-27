c8d8b231fbaa84ef9e11a05cb59260ca334d2a2e
PinkHash <sidorchuk19997@gmail.com>
2024-06-28 00:00:27 +0300

    Add js to our-services

---
+++ js/our-services.js
@@ -0,0 +1,14 @@
+function init() {
+  import("./global.header.js");
+  import("./our-services.spectacular-destinations-lightbox.js");
+}
+
+const totalPartials = document.querySelectorAll(
+  '[hx-trigger="load"], [data-hx-trigger="load"]'
+).length;
+let loadedPartialsCount = 0;
+
+document.body.addEventListener("htmx:afterOnLoad", () => {
+  loadedPartialsCount++;
+  if (loadedPartialsCount === totalPartials) init();
+});
---
+++ js/our-services.spectacular-destinations-lightbox.js
@@ -0,0 +1,57 @@
+const images = document.querySelectorAll(".destination-place__image");
+const lightbox = document.querySelector(".destinations__lightbox");
+const lightboxImage = document.querySelector(".destinations__lightbox-image");
+const prevButton = document.querySelector(".destinations__lightbox__btn-prev");
+const nextButton = document.querySelector(".destinations__lightbox__btn-next");
+let currentIndex = 0;
+
+function updateLightbox(index) {
+  const imageSrc = images[index].src;
+  lightboxImage.src = imageSrc;
+  currentIndex = index;
+
+  prevButton.style.visibility = currentIndex === 0 ? "hidden" : "visible";
+  nextButton.style.visibility =
+    currentIndex === images.length - 1 ? "hidden" : "visible";
+}
+
+images.forEach((image, index) => {
+  image.addEventListener("click", () => {
+    updateLightbox(index);
+    lightbox.style.display = "flex";
+  });
+});
+
+lightbox.addEventListener("click", (event) => {
+  if (!(event.target === prevButton || event.target === nextButton)) {
+    lightbox.style.display = "none";
+  }
+});
+
+prevButton.addEventListener("click", () => {
+  if (currentIndex > 0) {
+    updateLightbox(currentIndex - 1);
+  }
+});
+
+nextButton.addEventListener("click", () => {
+  if (currentIndex < images.length - 1) {
+    updateLightbox(currentIndex + 1);
+  }
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    lightbox.style.display = "none";
+  }
+  else if (event.key === "ArrowLeft"){
+    if (currentIndex > 0) {
+        updateLightbox(currentIndex - 1);
+      }
+  }
+  else if (event.key === "ArrowRight"){
+    if (currentIndex < images.length - 1) {
+        updateLightbox(currentIndex + 1);
+      }
+  }
+});
