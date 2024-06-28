021fe449659b0cf882821ba9ad8bdd66b9bf9e3d
AnnaSosn <Anchutiksos@gmail.com>
2024-06-28 11:44:58 +0000

    addJs Gallery

---
+++ js/gallery-section.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const images = document.querySelectorAll('.gallery-img img');
+
+    images.forEach(image => {
+        image.addEventListener('mouseenter', function() {
+            image.style.transform = 'scale(1.1)';
+        });
+
+        image.addEventListener('mouseleave', function() {
+            image.style.transform = 'scale(1)';
+        });
+    });
+});
